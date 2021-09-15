8608ea169a0b458e05a98fa9b2b395040af58141
Andrew Casarsa <acasarsa@gmail.com>
2021-09-15 13:52:01 -0400

    Ex 1 and 1.1
    using state hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,21 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
+
+  // EXTRA CREDIT 1.1 Make the Greeting accept a prop called initialName and initialize the name state to that value. 
+  // Greeting component accepts whatever is passed to it and if nothing is passed it defaults to ''
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +25,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Andrew"/>
 }
 
 export default App
